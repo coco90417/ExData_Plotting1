37f2c9dfc3adce4cfebe1cd7721e2766213bcce6
coco <coco90417@users.noreply.github.com>
2017-06-21 16:04:57 -0400

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,24 @@
+
+# load data ---------------------------------------------------------------
+
+source("hw1.R")
+load("plotData.RData")
+
+
+# plot --------------------------------------------------------------------
+
+png(filename = "plot2.png",
+    width = 480,
+    height = 480)
+
+plot(as_datetime(paste(plotData$DateNew, plotData$Time)),
+     plotData$Global_active_power,
+     type = "l", 
+     ylab = "Gloabl Active Power (kilowatts)", 
+     xlab = "")
+
+
+dev.off()
+
+
+
